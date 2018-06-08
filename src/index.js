d5dc31ec8dc68629f7b91a8db514fc6e3a4b63bf
cae <iyphir@gmail.com>
2018-06-08 13:23:54 -0400

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on('message', msg => {
+  if (msg.content === '~ping') {
+    msg.reply('pong!')
+  }
+  if (msg.channel === msg.guild.channels.find('name', 'check-in') && msg.content == '~done') {
+    msg.author.setRoles([msg.guild.roles.find('name','birds').id])
+  }
+  if (msg.content.startsWith('~color')) {
+    let content = msg.content.split(' ')[1];
+    let id = msg.author.id
+    if (content.charAt(0) != "#") content = '#' + content
+    if (!msg.guild.roles.find('name', id)) {
+      msg.guild.createRole({name: id})
+      msg.author.addRole(msg.guild.roles.find('name', id))
+    }
+    msg.guild.roles.find('name', id).setColor(content).catch(
+      msg.reply("Invalid color.")
+    );
+  }
+  if (mg.content.startsWith('~help')) {
+    msg.reply("Hello! I'm the helper bird! cae coded me. I gate the server and let you set your own color! Try `~color #HEXCODE`!")
+  }
+});
+
+client.login('token')
